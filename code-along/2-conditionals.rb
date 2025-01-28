16177d1c5d5b17cafeaefa5a3a2a0201d163dc8c
Reed Schultz <reed.schultz@kellogg.northwestern.edu>
2025-01-28 20:50:48 +0000

    did conditionals and arrays

--- code-along/2-conditionals.rb
+++ code-along/2-conditionals.rb
@@ -3,13 +3,70 @@
 # ruby 2-conditionals.rb
 
 # Booleans
+# test_is_true = true
+# puts test_is_true
 
-# Boolean Expressions
+# test_is_false= false
+# puts test_is_false 
 
+# # Boolean Expressions
+
+# puts 3 == 2
+# puts 3 !=2
+# puts 3 > 2
+# puts 3 < 2
 # If Conditional Logic
 
+# if 3 == 2
+#     puts "This text should never display"
+# end 
+
+# if 3 > 2
+#     puts "This text should always display"
+# end 
+
+# if 3==2
+#      puts "This text should always display"
+#  else 
+#     puts "This text should always display"
+# end 
 # If/Else Conditional Logic
+
+# user_entered_password = "tacos"
+# real_password = "secret"
+
+# if user_entered_password == real_password
+#     puts "Your password matches. Welcome Back"
+# else 
+#     puts "Wrong Password."
+# end 
+
 
 # Elsif Conditional Logic
 
+your_team_score = 2
+other_team_score = 2
+
+# if your_team_score > other_team_score
+#     puts "You Won"
+# elsif
+#     puts "Tied"
+# else 
+#     puts "Your Team Lost"
+# end 
+
 # Combining Expressions
+
+temp = 75
+precipitation = 0 
+
+# assign variables
+# taco = "tacos"
+
+#evaluate variables
+# "tacos" == taco
+
+if temp >= 65 && temp <= 75 && precipitation ==0
+    puts "It's perfect outside."
+else puts "It's not perfect outside."
+end 
--- code-along/3-arrays.rb
+++ code-along/3-arrays.rb
@@ -4,9 +4,44 @@
 
 # Arrays are lists of things
 
+favorite_foods = ["tacos", "pizza", "ice cream"]
+# puts favorite_foods
+# p favorite_foods
+
+
+# we_have_to_go_back = [4,8,15,16,23,42]
+# puts we_have_to_go_back
+# p we_have_to_go_back
+
+# mixed_array = ["tacos", 12, true]
+# p mixed_array
+# p mixed_array.length
+
 # Accessing the array
 
+# puts mixed_array[2]
+# puts mixed_array[-2]
+
+# shopping_lists = [["milk", "eggs", "toilet paper",], ["soap", "shampoo", "shaving cream"]]
+# p shopping_lists
+
+# p shopping_lists[1][2]
+
 # Add to the array
+
+# favorite_foods << "more tacos"
+# puts favorite_foods
+
+favorite_foods.push "more tacos"
+# puts favorite_foods
+# p favorite_foods
+
+favorite_foods= favorite_foods + ["french fries", "ramen"]
+p favorite_foods
+
+puts favorite_foods.count
+puts favorite_foods.size
+puts favorite_foods.length
 
 # There are lots of fun things you can do with arrays:
 # https://ruby-doc.org/core-2.7.0/Array.html
